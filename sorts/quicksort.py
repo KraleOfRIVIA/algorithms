0585117684ba5dde175dd5a4284107654a654b4a
Cahir <artur08062003@mail.ru>
2023-06-04 03:31:52 +0600

    learning algorithms

--- sorts/quicksort.py
+++ sorts/quicksort.py
@@ -1,9 +1,10 @@
+import random
 def quicksort(array):
     if len(array) < 2:
         return array
     else:
-        pivot = array[0]
-        less = [i for i in array[1:] if i<=pivot]
-        greater = [i for i in array[1:] if i>pivot]
+        pivot = random.choice(array) # берем случайный элемент как опорный для улучшения среднего времени работы
+        less = [i for i in array if i<pivot]
+        greater = [i for i in array if i>pivot]
     return quicksort(less)+ [pivot] + quicksort(greater)
 print(quicksort([4,3,7,10,15,60]))
